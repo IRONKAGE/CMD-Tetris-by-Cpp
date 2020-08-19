6a14a35d90a960b195bf60e348fa58f1770b3ae1
IRONKAGE <IRONKAGE@gmail.com>
2020-08-20 01:27:51 +0300

    add Key Tester

---
+++ ConsolEngine/KeyTester.cpp
@@ -0,0 +1,19 @@
+//#include<iostream>
+//#include<conio.h>
+//using namespace std;
+//int main()
+//{
+//	char key_press;
+//	int ascii_value;
+//	cout << "\n\t\t\tpress any key to check  its ascii value\n\n\t\t\tpress esc to exit\n\n\n";
+//	while (1)
+//	{
+//		int esckey = 27;
+//		key_press = getch();
+//		ascii_value = key_press;
+//		if (ascii_value == esckey)
+//			break;
+//		cout << "\t\t\tkey pressed-> \" " << key_press << " \" ascii value =  " << ascii_value << "\n\n";
+//	}
+//	return 0;
+//}
